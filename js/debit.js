d511d0934875b9dc8d7db5842c6181cca0cf961b
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2022-07-18 17:12:41 +0300

    cancel btn

--- js/debit.js
+++ js/debit.js
@@ -1,7 +1,12 @@
-document
-  .querySelectorAll(".input-container[data-error] .input")
-  .forEach((inpEl) => {
-    inpEl.addEventListener("input", () =>
-      inpEl.parentElement.removeAttribute("data-error")
-    );
+function showicon() {
+  const input = document.querySelector(".input").value;
+  const cancel = document.querySelector(".delete-icon");
+
+  if (input.lenght <= 0) document.body.classList.remove("active");
+  else document.body.classList.add("active");
+
+  cancel.addEventListener("click", () => {
+    document.querySelector(".input").value = "";
+    document.body.classList.add("active");
   });
+}
