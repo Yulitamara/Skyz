7f54a30f9386b91d08150fc49f442bcd9439570c
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2022-07-19 16:52:36 +0300

    interactive-demo start

--- js/debit.js
+++ js/debit.js
@@ -4,4 +4,37 @@ btnClear.addEventListener("click", () => {
   inputs.forEach((input) => (input.value = ""));
 });
 
-    
+// Configuration
+// Message
+$(function () {
+  $(".message").hide();
+});
+
+// $(document).ready(function () {
+//   $("#message").click(function () {
+//     $(".message").toggle();
+//   });
+// });
+
+$("#message").click(function () {
+  $(".message").toggle();
+});
+
+// Rquired
+$(function () {
+  $(".rquired").hide();
+});
+
+$("#rquired").click(function () {
+  $(".rquired").toggle();
+});
+
+// Narrow
+$("#narrow").click(function () {
+  $(".narrow").toggle();
+});
+
+// Flex direction
+$("#flex-direction").click(function () {
+  $(".form-container").css("flex-direction", "column");
+});
